d959ee65e335da515bc852f88cafde7d2660caed
ALI-Technical <ali.aa251104@gmail.com>
2024-06-09 23:29:39 +0500

    next app with internationalization - app dir

---
+++ src/app/not-found.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react'
+
+export function NotFound() {
+    return (
+        <html>
+            <body className='text-center'>
+                <h1 className='mt-10 font-semibold'>Something went wrong</h1>
+            </body>
+        </html>
+    )
+}
+
+export default NotFound;
